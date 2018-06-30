d1c710ef65529a000c443cf89d993087610dd8d5
Eric Andersson <eric.andersson88@outlook.com>
2018-07-01 01:05:53 +0200

    Fix some linting issues

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,36 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-// class Video extends Component {
-//   render() {
-//     const { src } = this.props
-//     console.log(this.props)
-//     return (
-//       <div>
-//         <video key={src} width="100%" height="100%" controls>
-//           <source src={src} type="video/mp4" />
-//           Your browser does not support the video tag.
-//         </video>
-//       </div>
-//     )
-//   }
-// }
+const Video = ({ src }) => (
+  <div>
+    <video key={src} width="100%" height="100%" controls>
+      <source src={src} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </div>
+)
 
-const Video = ({ src }) => {
-  return (
-    <div>
-      <video muted key={src} width="100%" height="100%" controls>
-        <source src={src} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-  )
-}
-
-
-
-const mapStateToProps = (state) => {
-  return { src: state.activeVideo.src }
-}
+const mapStateToProps = state => ({ src: state.activeVideo.src })
 
 export default connect(mapStateToProps)(Video)
